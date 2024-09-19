635635868387bddf7bf903df4e39b1c463d4e1a3
Paweł Krok <kadetkroksapsp2@gmail.com>
2024-09-19 12:23:49 +0200

    Create game logic without checking the winner
    Console game logic has been created. Handling of victory rules has not yet been implemented.

--- script.js
+++ script.js
@@ -9,6 +9,23 @@ function Board() {
             board[i].push(Cell());
         }
     }
+
+    const getBoard = () => board;
+
+    const markCell = (row, column, player) => {
+        const availableCell = board[row][column];
+
+        if (availableCell.getValue()) return 0;
+        availableCell.addMark(player)
+        return 1;
+    };
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+    };
+
+    return { getBoard, markCell, printBoard };
 }
 
 function Cell() {
@@ -25,3 +42,57 @@ function Cell() {
         getValue
     };
 }
+
+function GameController(
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+) {
+    const board = Board();
+
+    const players = [
+        {
+            name: playerOneName,
+            mark: "◯"
+        },
+        {
+            name: playerTwoName,
+            mark: "X"
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+    };
+
+    const playRound = (row, column) => {
+        console.log(
+            `The ${getActivePlayer().name}  selects cell ${row} ${column}...`
+        );
+        if (board.markCell(row, column, getActivePlayer().mark)) {
+
+            /*  This is where we would check for a winner and handle that logic,
+                such as a win message. */
+
+            switchPlayerTurn();
+            printNewRound();
+        } 
+    };
+
+    printNewRound();
+
+    return {
+        playRound,
+        getActivePlayer
+    };
+}
+
+const game = GameController();
